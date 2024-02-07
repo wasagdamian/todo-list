53c291b08ca8a4bb3494c150cf34291c6f5832f6
damianwasag <damian.wasag@ttsw.com.pl>
2024-02-07 12:45:44 +0100

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,23 @@ export class AppComponent {
     lato: 'summer'
   };
   price = 213.23
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2024-02-07',
+      done: false,
+    },
+    {
+      name: 'Zakupy',
+      deadline: '2024-02-08',
+      done: false,
+    },
+    {
+      name: 'Basen',
+      deadline: '2024-02-09',
+      done: false,
+    },
+  ];
 
   get footer(): string {
     return ' © Lista zadań zbudowana w Angularze.'

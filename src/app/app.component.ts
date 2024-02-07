bf916d5df092759aa3638f5068bd8a0d20e4bb36
damianwasag <damian.wasag@ttsw.com.pl>
2024-02-07 09:42:34 +0100

    currency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lista zadań';
-  num = 845.4564563
+  num = 845.4564563;
   translate = {
     zima: 'winter',
     wiosna: 'spring',
     jesień: 'autumn',
     lato: 'summer'
-  }
+  };
+  price = 213.23
 
   get footer(): string {
     return ' © Lista zadań zbudowana w Angularze.'
